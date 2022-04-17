61856e3838fef52c122a031d67e9825dfb8da70f
arshamalh <arshamalh@gmail.com>
2022-04-17 15:57:42 +0430

    testing commits.

--- client.go
+++ client.go
@@ -1,14 +1,27 @@
 package twigo
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/mrjones/oauth"
+)
+
+const (
+	base_url = "https://api.twitter.com/2/"
+)
 
 type Client struct {
-	APIKey          string
-	APISecret       string
-	BearerToken     string
-	AccessToken     string
-	AccessSecret    string
-	waitOnRateLimit bool
+	authorizedClient  *http.Client
+	consumerKey       string
+	consumerSecret    string
+	accessToken       string
+	accessTokenSecret string
+	bearerToken       string
+	read_only_access  bool
+	bearer_token_auth bool
 }
 
 type Response struct {
@@ -18,37 +31,72 @@ type Response struct {
 	Meta     interface{}
 }
 
-func NewClient(api_key, api_secret, bearer_token, access_token, access_secret string, wait_on_rate_limit bool) *Client {
-	return &Client{
-		APIKey:          api_key,
-		APISecret:       api_secret,
-		BearerToken:     bearer_token,
-		AccessToken:     access_token,
-		AccessSecret:    access_secret,
-		waitOnRateLimit: wait_on_rate_limit,
+func NewClient(consumerKey, consumerSecret, accessToken, accessTokenSecret, bearerToken string, wait_on_rate_limit bool) (*Client, error) {
+	keys_exists := consumerKey != "" && consumerSecret != "" && accessToken != "" && accessTokenSecret != ""
+	bearer_token_auth := bearerToken != ""
+	read_only_access := bearer_token_auth && !keys_exists
+
+	if !read_only_access && !keys_exists {
+		return nil, fmt.Errorf("consumer key, consumer secret, access token and access token secret must be provided")
 	}
+
+	if !read_only_access {
+		consumer := oauth.NewConsumer(
+			consumerKey,
+			consumerSecret,
+			oauth.ServiceProvider{
+				RequestTokenUrl:   "https://api.twitter.com/oauth/request_token",
+				AuthorizeTokenUrl: "https://api.twitter.com/oauth/authorize",
+				AccessTokenUrl:    "https://api.twitter.com/oauth/access_token",
+			})
+		consumer.Debug(false)
+
+		t := oauth.AccessToken{
+			Token:  accessToken,
+			Secret: accessTokenSecret,
+		}
+
+		authorizedClient, err := consumer.MakeHttpClient(&t)
+		return &Client{authorizedClient, consumerKey, consumerSecret, accessToken, accessTokenSecret, bearerToken, read_only_access, bearer_token_auth}, err
+	}
+
+	return &Client{nil, consumerKey, consumerSecret, accessToken, accessTokenSecret, bearerToken, read_only_access, bearer_token_auth}, nil
 }
 
-func (client *Client) make_request(method, url string, params map[string]string, body interface{}, user_auth bool) (*Response, error) {
-	// base_url := "https://api.twitter.com"
-	headers := map[string]string{"User-Agent": "twigo"}
-	if user_auth {
-		headers["Authorization"] = fmt.Sprintf("Basic %s", client.AccessToken)
-	} else {
-		headers["Authorization"] = fmt.Sprintf("Bearer %s", client.BearerToken)
+// func (c *Client) request(method, url string, params map[string]string, body interface{}, user_auth bool) (*Response, error) {
+// 	return nil, nil
+// }
+
+func (c *Client) GetUserTweets(user_id string) (*http.Response, error) {
+	response, err := c.authorizedClient.Get(fmt.Sprintf("%s/users/%s/tweets", base_url, user_id))
+	return response, err
+}
+// tweet_id and target_user_id can be string or int
+func (c *Client) GetMe() *Response
+func (c *Client) CreateTweet(text string, options ...interface{}) (*Response, error) {
+	status := fmt.Sprintf("Test post via the API using Go (http://golang.org/) at %s", time.Now().String())
+
+	response, err := c.authorizedClient.PostForm(
+		"https://api.twitter.com/1.1/statuses/update.json",
+		url.Values{"status": []string{status}})
+
+	if err != nil {
+		return nil, err
 	}
+
+	fmt.Printf("%v\n", response)
 	return nil, nil
 }
 
-func (client *Client) GetMe() *Response
-func (client *Client) CreateTweet(text string, options ...interface{}) *Response
+func (c *Client) Like(tweet_id string, options ...interface{}) *Response
+func (c *Client) Retweet(tweet_id string, options ...interface{}) *Response
+func (c *Client) SearchAllTweets(query string, options ...interface{}) *Response
+func (c *Client) FollowUser(target_user_id string, options ...interface{}) *Response
+func (c *Client) UnfollowUser(target_user_id string, options ...interface{}) *Response
+func (c *Client) Unlike(tweet_id string, options ...interface{}) *Response
+func (c *Client) Unretweet(tweet_id string, options ...interface{}) *Response
 
-// tweet_id and target_user_id can be string or int
-func (client *Client) Like(tweet_id string, options ...interface{}) *Response
-func (client *Client) Retweet(tweet_id string, options ...interface{}) *Response
-
-func (client *Client) SearchAllTweets(query string, options ...interface{}) *Response
-func (client *Client) FollowUser(target_user_id string, options ...interface{}) *Response
-func (client *Client) UnfollowUser(target_user_id string, options ...interface{}) *Response
-func (client *Client) Unlike(tweet_id string, options ...interface{}) *Response
-func (client *Client) Unretweet(tweet_id string, options ...interface{}) *Response
+// Some wants only bearer
+// Some wants only consumer
+// Some (writes) only can use consumer
+// Some (gets) is better to use bearer
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/arshamalh/twigo
 
 go 1.18
+
+require github.com/mrjones/oauth v0.0.0-20190623134757-126b35219450
