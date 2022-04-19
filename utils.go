0aaf275389ae300fda496c485c02f8911ed50bb3
arshamalh <arshamalh@gmail.com>
2022-04-20 01:19:16 +0430

    Some name changes and get_request method now get params (limited).

---
+++ utils.go
@@ -0,0 +1,11 @@
+package twigo
+
+// Does an array of strings contain an especial string?
+func contains(arrayOfStrings []string, string_item string) bool {
+    for _, val := range arrayOfStrings {
+        if val == string_item {
+            return true
+        }
+    }
+    return false
+}
