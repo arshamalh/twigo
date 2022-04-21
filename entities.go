6654249ede52780e593b8b88cc619a941b252c81
arshamalh <arshamalh@gmail.com>
2022-04-21 19:21:46 +0430

    RateLimits added to response.

--- entities.go
+++ entities.go
@@ -3,6 +3,7 @@ package twigo
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 )
 
 // *** Basic Entities *** //
@@ -20,16 +21,28 @@ type ErrorEntity struct {
 }
 
 type IncludesEntity struct {
-	Users []User `json:"users"`
+	Users  []User  `json:"users"`
 	Tweets []Tweet `json:"tweets"`
-	Polls []Poll `json:"polls"` // Do you remember if "poll" in the client?
+	Polls  []Poll  `json:"polls"` // Do you remember if "poll" in the client?
 	Places []Place `json:"places"`
-	Media []Media `json:"media"`
+	Media  []Media `json:"media"`
 }
 
-type Poll struct {}
-type Place struct {}
-type Media struct {}
+type RateLimits struct {
+	Limit          int `json:"x-rate-limit"`
+	Remaining      int `json:"x-rate-limit-remaining"`
+	ResetTimestamp int `json:"x-rate-limit-reset"`
+}
+
+func (r *RateLimits) Set(header http.Header) {
+	r.Limit, _ = strconv.Atoi(header.Get("X-Rate-Limit-Limit"))
+	r.Remaining, _ = strconv.Atoi(header.Get("X-Rate-Limit-Remaining"))
+	r.ResetTimestamp, _ = strconv.Atoi(header.Get("X-Rate-Limit-Reset"))
+}
+
+type Poll struct{}
+type Place struct{}
+type Media struct{}
 type Tweet struct {
 	ID               string   `json:"id"`
 	Text             string   `json:"text"`
@@ -87,105 +100,121 @@ type List struct {
 
 // *** Response Entities *** //
 type TweetResponse struct {
-	Data     Tweet
-	Includes IncludesEntity
-	Errors   []ErrorEntity
-	Meta     MetaEntity
+	Data       Tweet
+	Includes   IncludesEntity
+	Errors     []ErrorEntity
+	Meta       MetaEntity
+	RateLimits RateLimits
 }
 
 func (r *TweetResponse) Parse(raw_response *http.Response) (*TweetResponse, error) {
 	err := json.NewDecoder(raw_response.Body).Decode(&r)
 	defer raw_response.Body.Close()
+	r.RateLimits.Set(raw_response.Header)
 	return r, err
 }
 
 type TweetsResponse struct {
-	Data     []Tweet
-	Includes IncludesEntity
-	Errors   []ErrorEntity
-	Meta     MetaEntity
+	Data       []Tweet
+	Includes   IncludesEntity
+	Errors     []ErrorEntity
+	Meta       MetaEntity
+	RateLimits RateLimits
 }
 
 func (r *TweetsResponse) Parse(raw_response *http.Response) (*TweetsResponse, error) {
 	err := json.NewDecoder(raw_response.Body).Decode(&r)
 	defer raw_response.Body.Close()
+	r.RateLimits.Set(raw_response.Header)
 	return r, err
 }
 
 type UserResponse struct {
-	Data     User
-	Includes IncludesEntity
-	Errors   []ErrorEntity
-	Meta     MetaEntity
+	Data       User
+	Includes   IncludesEntity
+	Errors     []ErrorEntity
+	Meta       MetaEntity
+	RateLimits RateLimits
 }
 
 func (r *UserResponse) Parse(raw_response *http.Response) (*UserResponse, error) {
 	err := json.NewDecoder(raw_response.Body).Decode(&r)
 	defer raw_response.Body.Close()
+	r.RateLimits.Set(raw_response.Header)
 	return r, err
 }
 
 type UsersResponse struct {
-	Data     []User
-	Includes IncludesEntity
-	Errors   []ErrorEntity
-	Meta     MetaEntity
+	Data       []User
+	Includes   IncludesEntity
+	Errors     []ErrorEntity
+	Meta       MetaEntity
+	RateLimits RateLimits
 }
 
 func (r *UsersResponse) Parse(raw_response *http.Response) (*UsersResponse, error) {
 	err := json.NewDecoder(raw_response.Body).Decode(&r)
 	defer raw_response.Body.Close()
+	r.RateLimits.Set(raw_response.Header)
 	return r, err
 }
 
 type SpaceResponse struct {
-	Data     Space
-	Includes IncludesEntity
-	Errors   []ErrorEntity
-	Meta     MetaEntity
+	Data       Space
+	Includes   IncludesEntity
+	Errors     []ErrorEntity
+	Meta       MetaEntity
+	RateLimits RateLimits
 }
 
 func (r *SpaceResponse) Parse(raw_response *http.Response) (*SpaceResponse, error) {
 	err := json.NewDecoder(raw_response.Body).Decode(&r)
 	defer raw_response.Body.Close()
+	r.RateLimits.Set(raw_response.Header)
 	return r, err
 }
 
 type SpacesResponse struct {
-	Data     Space
-	Includes IncludesEntity
-	Errors   []ErrorEntity
-	Meta     MetaEntity
+	Data       Space
+	Includes   IncludesEntity
+	Errors     []ErrorEntity
+	Meta       MetaEntity
+	RateLimits RateLimits
 }
 
 func (r *SpacesResponse) Parse(raw_response *http.Response) (*SpacesResponse, error) {
 	err := json.NewDecoder(raw_response.Body).Decode(&r)
 	defer raw_response.Body.Close()
+	r.RateLimits.Set(raw_response.Header)
 	return r, err
 }
 
 type ListResponse struct {
-	Data     List
-	Includes IncludesEntity
-	Errors   []ErrorEntity
-	Meta     MetaEntity
+	Data       List
+	Includes   IncludesEntity
+	Errors     []ErrorEntity
+	Meta       MetaEntity
+	RateLimits RateLimits
 }
 
 func (r *ListResponse) Parse(raw_response *http.Response) (*ListResponse, error) {
 	err := json.NewDecoder(raw_response.Body).Decode(&r)
 	defer raw_response.Body.Close()
+	r.RateLimits.Set(raw_response.Header)
 	return r, err
 }
 
 type ListsResponse struct {
-	Data     List
-	Includes IncludesEntity
-	Errors   []ErrorEntity
-	Meta     MetaEntity
+	Data       List
+	Includes   IncludesEntity
+	Errors     []ErrorEntity
+	Meta       MetaEntity
+	RateLimits RateLimits
 }
 
 func (r *ListsResponse) Parse(raw_response *http.Response) (*ListsResponse, error) {
 	err := json.NewDecoder(raw_response.Body).Decode(&r)
 	defer raw_response.Body.Close()
+	r.RateLimits.Set(raw_response.Header)
 	return r, err
 }
